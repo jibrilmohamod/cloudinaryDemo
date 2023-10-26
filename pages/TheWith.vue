496a6a1aa816e179d108ac9b153da3fed09ee2c3
jibril <jazimjibril@gmail.com>
2023-10-26 18:21:01 +0300

    Made changes ⚡

--- pages/TheWith.vue
+++ pages/TheWith.vue
@@ -32,7 +32,44 @@
              src="v1673946334/cld-sample-5.jpg"
              sizes="sm:100vw md:50vw lg:70vw xl:50vw"
              loading="lazy"
+             :placeholder="15"
              format="webp" />
+
+    <h1>
+        Defined Height and Width
+    </h1>
+    <NuxtImg provider="cloudinary"
+             src="v1673946334/cld-sample-5.jpg"
+             sizes="sm:100vw md:50vw lg:70vw xl:50vw"
+             loading="lazy"
+             height="600px"
+             width="600px"
+             format="webp" />
+
+    <h1>
+        Quality
+    </h1>
+    <NuxtImg provider="cloudinary"
+             src="v1673946334/cld-sample-5.jpg"
+             sizes="sm:100vw md:50vw lg:70vw xl:50vw"
+             loading="lazy"
+             height="600px"
+             width="600px"
+             quality="10"
+             format="webp" />
+
+    <h1>
+        <NuxtImg provider="cloudinary"
+                 src="v1673946334/cld-sample-5.jpg"
+                 sizes="sm:100vw md:50vw lg:70vw xl:50vw"
+                 loading="lazy"
+                 height="600px"
+                 width="600px"
+                 quality="60"
+                 format="webp"
+                 :modifiers="{ roundCorner: '0:100' }" />
+
+    </h1>
 </template>
 
 <script setup>
