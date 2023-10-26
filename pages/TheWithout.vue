009ef7ebdfa4b8da2184242df00aa18e6919aa6a
jibril <jazimjibril@gmail.com>
2023-10-26 19:07:17 +0300

    Los

--- pages/TheWithout.vue
+++ pages/TheWithout.vue
@@ -2,6 +2,9 @@
     <div>
         without cloudinary
 
+        <img src="https://res.cloudinary.com/dckmlqzgd/image/upload/v1698334630/Pizigani_1367_Chart_10MB_ptvflp.jpg"
+             alt=""
+             style="width: fit-content ; height: 100vh; background-color: red;">
 
         <button>
             <nuxt-link to="/">
